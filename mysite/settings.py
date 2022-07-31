a69d6d4a9666e64d3412addbea2fcd203b885228
Aqzhol93 <akzholzhaksigeldy@gmail.com>
2022-07-31 15:13:06 +0600

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2l*q1+%*=@lo3#l)z69%n5$t+a^q(s#nt+b-+@eon_ddb6w(9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
